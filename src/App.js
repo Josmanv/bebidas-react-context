524e020951a11dcc68b74b7a4e9b8191a32c19fc
josmanv <josem_lc@hotmail.com>
2021-06-02 23:43:07 +0200

    [add] Mostrando los primeros resultados

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, {Fragment} from 'react';
 import Header from './components/Header';
 import Footer from './components/Footer';
 import Formulario from './components/Formulario';
+import ListaRecetas from './components/ListaRecetas';
 
 import CategoriasProvider from './context/CategoriasContext';
 import RecetasProvider from './context/RecetasContext';
@@ -15,6 +16,7 @@ function App() {
         <div className="row">
           <Formulario />
         </div>
+        <ListaRecetas />
       </div>
       <Footer />
     </RecetasProvider>
---
+++ src/components/Receta.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const Receta = ({receta}) => {
+
+    const {strDrink, strDrinkThumb} = receta;
+
+    return ( 
+        <div className="col-md-4 mb-3">
+            <div className="card">
+                <img className="card-img-top" src={strDrinkThumb} alt={strDrink} />
+                <div className="card-body">
+                    <p className="card-text">{strDrink}</p>
+                </div>
+            </div>
+
+            
+        </div>
+     );
+}
+ 
+export default Receta;
