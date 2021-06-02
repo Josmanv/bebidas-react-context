efdede2312c961e094bd43e115dcddb339977343
josmanv <josem_lc@hotmail.com>
2021-06-02 23:53:47 +0200

    [upd] Mostrando los primeros resultados

--- src/components/Receta.js
+++ src/components/Receta.js
@@ -7,9 +7,15 @@ const Receta = ({receta}) => {
     return ( 
         <div className="col-md-4 mb-3">
             <div className="card">
-                <img className="card-img-top" src={strDrinkThumb} alt={strDrink} />
+                <img className="card-img-top" src={strDrinkThumb} alt={`Imagen de ${strDrink}`} />
                 <div className="card-body">
                     <p className="card-text">{strDrink}</p>
+                    <button
+                        type="button"
+                        className="btn btn-block btn-outline-info"
+                    >
+                    Ver Receta
+                    </button>
                 </div>
             </div>
 
